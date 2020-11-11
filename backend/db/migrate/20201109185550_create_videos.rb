9de28a61df828736d9444eb3c1c957b6af8cbfb8
Amonsta <amana.ammi@gmail.com>
2020-11-11 15:36:39 -0700

    first migration and seed data

--- backend/db/migrate/20201109185550_create_videos.rb
+++ backend/db/migrate/20201109185550_create_videos.rb
@@ -1,10 +1,11 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
     create_table :videos do |t|
-      t.string :name
+      t.string :title
       t.string :vid_url
       t.string :mood
       t.string :genre
+      t.belongs_to :room, null: false, foreign_key: true
 
       t.timestamps
     end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    blue = Room.create(name: "Blue Room", image_url: "https://www.pexels.com/photo/shallow-focus-photography-of-water-droplets-1202841/")
+
+    Video.create([
+        { title: "Rainy Day Soul", vid_url: "https://www.youtube.com/watch?v=f02mOEt11OQ", mood: "chill", genre: "Soul/R&B", room_id: blue.id}
+    ])

492290c87e869b06de237a188e9b3190c27ecb2c
Amonsta <amana.ammi@gmail.com>
2020-11-12 18:21:10 -0700

    Home page styling

---
+++ react-client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.css';
+
+class Home extends React.Component {
+    render(){
+
+        return (
+          <div>
+            <h1 className="logo">The Mood Room</h1>
+          </div>
+        )
+    }
+}
+
+export default Home

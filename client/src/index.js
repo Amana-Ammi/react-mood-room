d879e9b638e245695afbf9594f58d6e1e917664a
Amonsta <amana.ammi@gmail.com>
2020-11-15 23:12:32 -0700

    add redux  to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { thunk } from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

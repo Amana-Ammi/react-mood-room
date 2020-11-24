bafb81eb59d1f189fb6b9eea85c8b178cbe71238
Amonsta <amana.ammi@gmail.com>
2020-11-24 11:25:42 -0700

    add description

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,17 +9,21 @@
 
 
 Video.create([
-    { title: "A Little Seoul", vid_url: "https://youtu.be/39S4BAvjFeo", mood: "chill", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/Dance.jpg", description: "A few introductory Korean R&B tracks that are sure to get you grooving." }
+    { title: "A Little Seoul", vid_url: "https://youtu.be/39S4BAvjFeo", mood: "Chill", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/Dance.jpg", description: "A few introductory Korean R&B tracks that are sure to get you grooving." }
 ])
 
 Video.create([
-    { title: "Wu-Tang Generator Name", vid_url: "https://www.youtube.com/watch?v=ExVtrghW5Y4&list=PLzDWYEiYiW2Avuzbt0Pz2dPPXRr3AHYsH", mood: "hype", image_url: "https://free2music.com/images/singer/2018/11/01/childish-gambino_0.jpg", description: "Childish Gambino. Need I say More?" }
+    { title: "Wu-Tang Generator Name", vid_url: "https://www.youtube.com/watch?v=ExVtrghW5Y4&list=PLzDWYEiYiW2Avuzbt0Pz2dPPXRr3AHYsH", mood: "Hype", image_url: "https://free2music.com/images/singer/2018/11/01/childish-gambino_0.jpg", description: "Childish Gambino. Need I say More?" }
 ])
 
 Video.create([
-    { title: "Afro Beats", vid_url: "https://youtu.be/SQ2cHyowuwI", mood: "happy", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/afro-beat.jpg", description: "A true happy place."}
+    { title: "MOOD FOREVER", vid_url: "https://www.youtube.com/watch?v=LXXQLa-5n5w&list=PLc5z3xejLEJL-Tmh2FWHRpGko3q43ZKMR", mood: "Beyonce", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/Beyonce.png", description: "Yes, Beyonce is a Mood." }
 ])
 
 Video.create([
-    { title: "Rainy Day", vid_url: "https://youtu.be/PReV3zH4iIw", mood: "relaxed", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/rain_1.jpg", description: "Sometimes you need to relax as if it was raining outside. Grab a blanket and some hot chocolate for this one." }
+    { title: "AFRO-BEATZ", vid_url: "https://youtu.be/SQ2cHyowuwI", mood: "Happy", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/afro-beat.jpg", description: "A true happy place."}
+])
+
+Video.create([
+    { title: "RAINY DAY SOUL", vid_url: "https://youtu.be/PReV3zH4iIw", mood: "Relaxed", image_url: "https://react-my-dj.s3-us-west-2.amazonaws.com/rain_1.jpg", description: "Sometimes you need to relax as if it was raining outside. Grab a blanket and some hot chocolate for this one." }
 ])

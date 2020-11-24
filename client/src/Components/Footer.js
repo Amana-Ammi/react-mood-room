1bebc35f3282bab1af27bd190249111b97c72993
Amonsta <amana.ammi@gmail.com>
2020-11-24 02:28:38 -0700

    add footer component and css file

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <h4 className="footer"> Thank you for Listening.. - Amonsta </h4>
+          );
+    }
+}
+ 
+export default Footer;

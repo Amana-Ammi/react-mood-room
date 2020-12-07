8b6b26346e071be04d25791ede57ccd44e8237d8
Amonsta <amana.ammi@gmail.com>
2020-12-06 21:02:29 -0700

    add about me component

---
+++ client/src/Components/About.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1> MEET THE CURATOR</h1>
+            <h3> Amonsta a.k.a. Amana </h3>
+            <h4> Pronouns: she/her & they/them </h4>
+            
+            <p> 
+                Long story short, they took over the family business at the age of 21.
+                While on a business trip, they were involved in a horrendous explosion that nearly took their life. Ontop of that, they were taken captive by a warlord who wanted the company's product
+                for himself. 
+                Luckily, they met a fellow Engineer who helped them keep the shrapnel that was lodged near their heart from killing them.
+                Putting their minds together, they built the suit of a lifetime and embarked on the ultimate escape. 
+                Now, they like to play Metallica whenever they enter a room.
+            </p>
+            <p>
+                Kidding! That's Iron Man, silly!
+
+                Although Amonsta does enjoy Metallica, her story isn't quite as dangerous as Tony Starks.
+                
+
+                Born an Aquarius in the glorious month of February, music has always been the root of her personality. From charging friends in elementary school
+                for printouts of lyrics to making exquisite mixtapes on her Walkman. Amonsta even dabbled in DJing while in college. Filling School Conference After Parties with the vibes of a lifetime
+                and even creating the flow for her friend's Legendary Drag Show performances.
+            </p>
+            <p>
+                They have become no stranger to the "can you send me new music?" question. It gets overwhelming at times. That's when they decided to create The Mood Room.
+                A place unlike your typical streaming app. However, still a place where they can show off new playlists of songs without any barriers to their creativity.
+
+                Here, Amonsta can create visuals to go with the mixes she's creating. A way to truly embed the vibes she's hoping to share with others.
+
+                It is her hope that you vibe as much in The Mood Room as she has. 
+
+                Enjoy!
+
+                Amonsta looks forward to sharing even more with you as The Mood Room grows.
+
+                Love & Light!
+            </p>
+        </div>
+    )
+}
+
+export default About;
